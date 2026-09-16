538caa45a4cb2fa0b041129f4ec0c2514cdb70b8
ag11487 <ag11487@email.vccs.edu>
2026-09-16 19:13:42 -0400

    Create PatientName.cs
    patient name assignment

---
+++ PatientName.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        string patientNname;
+
+        Console.WriteLine("Who is being seen today?");
+        patientNname = Console.ReadLine();
+    
+        Console.WriteLine("Welcome: " + patientNname);
+    }
+}
